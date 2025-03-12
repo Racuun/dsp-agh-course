122c2490b1ff4da63275540e3ee0d8e352d00ec6
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2025-03-12 15:56:19 +0100

    fix: :bug: fix square impulse

--- part3/ex5.jl
+++ part3/ex5.jl
@@ -1,8 +1,8 @@
 function ci_rectangular(t; T=1)::Float64
-    t>0 && return (t÷T)%2
+    t<=T && return 1
     return 0
 end
 
 for i in 0:0.1:2
-    display(ci_rectangular(i; T=2))
+    display(ci_rectangular(i; T=1))
 end
